52c73c437a1bcc0dcd472aa4136c927eb1327da7
Peter Chernoff <peter.chernoff@gmail.com>
2017-10-30 21:45:27 -0400

    Updating pcCreateRigUtilities to allow box creation, sets the geometry

--- pcCreateRigUtilities.py
+++ pcCreateRigUtilities.py
@@ -1,8 +1,8 @@
 import maya.cmds as mc
 
-class pcCreateRigUtilities(object):
-
-    def createCTRLs(self, s, size=3, prnt = False, ornt = False, pnt=False, orientVal=(1, 0, 0), colour=5, sectionsTU=None, addPrefix=False):
+class pcCreateRigUtilities:
+    @staticmethod
+    def createCTRLs(s, size=3, prnt = False, ornt = False, pnt=False, orientVal=(1, 0, 0), colour=5, sectionsTU=None, addPrefix=False, boxDimensionsLWH=None):
         selname = str(s)
         '''
         0 gray, 1 black, 2 dark grey, 3 light gray, 4 red
@@ -27,11 +27,25 @@ class pcCreateRigUtilities(object):
             ctrlName = "CTRL_" + selname
         else:
             ctrlName = selname.replace("JNT_", "CTRL_")
+        if boxDimensionsLWH:
+            x = boxDimensionsLWH[0]
+            y = boxDimensionsLWH[1]
+            z = boxDimensionsLWH[2]
 
-        if sectionsTU:
-            ctrl = mc.circle(nr=orientVal, r=size, n=ctrlName, degree=1, sections=sectionsTU)[0]
+            toPass = [(x, y, z), (-x, y, z), (-x, y, -z), (x, y, -z), (x, y, z),
+                      (x, -y, z), (-x, -y, z), (-x, y, z), (-x, -y, z),
+                      (-x, -y, -z), (-x, y, -z), (-x, -y, -z), (x, -y, -z),
+                      (x, y, -z), (x, -y, -z), (x, -y, z), ]
+            try:
+                ctrl = mc.curve(ctrlName, r=True, d=1, p=toPass,)
+            except:
+                ctrl = mc.curve(name=ctrlName, d=1, p=toPass,)
+            #ctrl = mc.curve(name=ctrlName, p=toPass, d=1)
         else:
-            ctrl = mc.circle(nr=orientVal, r=size, n=ctrlName)[0]
+            if sectionsTU:
+                ctrl = mc.circle(nr=orientVal, r=size, n=ctrlName, degree=1, sections=sectionsTU)[0]
+            else:
+                ctrl = mc.circle(nr=orientVal, r=size, n=ctrlName)[0]
 
         mc.setAttr('{0}.overrideEnabled'.format(ctrlName), 1)
         mc.setAttr("{0}.overrideColor".format(ctrlName), colour)
@@ -52,23 +66,31 @@ class pcCreateRigUtilities(object):
         offsetCtrl = [offset, ctrl, auto]
         return offsetCtrl
 
-    def lockHideCtrls(self, s, translate=False, rotate=False, scale=False, theVals = [], toHide = False, toLock = True):
-
+    @staticmethod
+    def lockHideCtrls(s, translate=False, rotate=False, scale=False, theVals = [], toHide = False, visible=False, toLock = True):
+        myVals = list(theVals) #need to reset it every time
         if translate:
-            theVals.extend(["tx", "ty", "tz"])
+            myVals.extend(["tx", "ty", "tz"])
         if rotate:
-
-            theVals.extend(["rx", "ry", "rz"])
+            myVals.extend(["rx", "ry", "rz"])
         if scale:
-            theVals.extend(["sx", "sy", "sz"])
-
-        for i in range(len(theVals)):
-            mc.setAttr("{0}.{1}".format(s, theVals[i]),k=toHide, l=toLock)
-
+            myVals.extend(["sx", "sy", "sz"])
+        if visible:
+            myVals.extend(["v"])
 
 
+        for i in range(len(myVals)):
+            mc.setAttr("{0}.{1}".format(s, myVals[i]),k=toHide, l=toLock)
+            # to delete this next part
+            if "_IK_" in s:
+                print("{0}.{1}".format(s, myVals[i]))
 
-    def setDriverDrivenValues(self, driver, driverAttribute, driven, drivenAttribute, driverValue, drivenValue, modifyInOut=None, modifyBoth=None):
+
+
+
+
+    @staticmethod
+    def setDriverDrivenValues(driver, driverAttribute, driven, drivenAttribute, driverValue, drivenValue, modifyInOut=None, modifyBoth=None):
         # the way it's written, the setDrivenKeyframe is driven-> driver, not the other way around. My custom value does the more intuitive manner
         # modify tanget is determining if the tanget goes in or out
         if modifyInOut or modifyBoth:
@@ -85,5 +107,39 @@ class pcCreateRigUtilities(object):
         else:
             mc.setDrivenKeyframe('{0}.{1}'.format(driven, drivenAttribute), cd='{0}.{1}'.format(driver, driverAttribute),
                              dv=driverValue, v=drivenValue)
+
+    @staticmethod
+    def createLocatorToDelete(createLocator=True):
+        #This is little more than a signal to tell me I need to undo what I've done
+        if createLocator:
+            toDelete = mc.spaceLocator(p=(0, 0, 0))[0]
+            mc.setAttr('{0}.overrideEnabled'.format(toDelete), 1)
+            mc.setAttr("{0}.overrideColor".format(toDelete), 13)
+
+    @staticmethod
+    def tgpSetGeo(geoJntArray, setter="JNT_", *args):
+        print(geoJntArray)
+        for i in range(len(geoJntArray)):
+            try:
+                #print("------")
+                theParent = geoJntArray[i]
+                #print(theParent)
+                geoName = theParent.replace(setter, "GEO_")
+                #print(geoName)
+                mc.parent(geoName, theParent)
+                #print("parent successful")
+                pivotTranslate = mc.xform(theParent, q=True, ws=True, rotatePivot=True)
+                #print("pivotTranslate Successful")
+                mc.makeIdentity(geoName, a=True, t=True, r=True, s=True)
+                #print("make identity Successful")
+                mc.xform(geoName, ws=True, pivots=pivotTranslate)
+                #print("xform Successful")
+
+            except:
+                mc.warning("Geo for {0} properly named or available".format(geoJntArray[i]))
+                mc.warning("===========")
+
+
+
 
 
