b83423de31eff04aee92accc381c459b620208a8
Peter Chernoff <peter.chernoff@gmail.com>
2017-11-23 00:03:09 -0500

    Adding cleanup

--- pcBindBonesToSet.py
+++ pcBindBonesToSet.py
@@ -2,12 +2,21 @@ import maya.cmds as mc
 nameSet = "bind_set"
 
 s1 = mc.ls('JNT*', type="joint")
-s2 = ['_FK_', '_IK_', 'End']
+s2 = ['_FK_', '_IK_', 'End', 'masterToes', 'ankleTwist']
 s3 = []
 for i in range(len(s1)):
     if not any(word in s1[i] for word in s2):
         s3.append((s1[i]))
+    if "IK_spine" in s1[i] and "End" not in s1[i]:
+        s3.append(s1[i])
+    if "FK_hip" in s1[i] and "End" not in s1[i]:
+        s3.append(s1[i])
+
 print(s1)
 print(s3)
-mc.sets(s3, n="bind_set")
+if mc.objExists(nameSet):
+
+    mc.sets(s3, include=nameSet)
+else:
+    mc.sets(s3, n=nameSet)
 mc.select(cl=True)
