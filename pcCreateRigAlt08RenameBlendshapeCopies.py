8ed7f5faf790a42fdc31b4c8ee651994eece7e2c
Peter Chernoff <peter.chernoff@gmail.com>
2018-05-25 18:03:28 -0400

    Adding the BlendSDK and RenameBlendShapeCopies

---
+++ pcCreateRigAlt08RenameBlendshapeCopies.py
@@ -0,0 +1,32 @@
+import maya.cmds as cmds
+
+mshChar = 'male_geo'
+
+history = cmds.listHistory(mshChar)
+# print("history {0}".format(history))
+blndName = cmds.ls(history, typ='blendShape')[0]
+# print("blndName {0}".format(blndName))
+blndVals = cmds.aliasAttr(blndName, q=True)
+# print("blndVals {0}".format(blndVals))
+
+for i in range(len(blndVals)):
+    if "_Copy" in blndVals[i][-5:]:
+        toRename = blndVals[i][:-5]
+        if "l_" in toRename[:2]:
+            toRename1 = toRename.replace("l_", "r_")
+            print("l to r {0}".format(toRename1))
+        elif "r_" in toRename[:2]:
+            toRename1 = toRename.replace("r_", "l_")
+            print("r to l {0}".format(toRename1))
+        print(" --- c ---")
+        # print (toRename1)
+        print ("{0}.{1}".format(blndName, blndVals[i]))
+        if cmds.objExists('{0}.{1}'.format(blndName, toRename1)):
+            cmds.aliasAttr('{0}.{1}'.format(blndName, toRename1), rm=True)
+        cmds.aliasAttr(toRename1, '{0}.{1}'.format(blndName, blndVals[i]))
+
+        # cmds.renameAttr( '{0}.{1}'.format(blndName, blndVals[i]), toRename1 )
+
+blndVals = cmds.aliasAttr(blndName, q=True)
+print("blndVals {0}".format(blndVals))
+
