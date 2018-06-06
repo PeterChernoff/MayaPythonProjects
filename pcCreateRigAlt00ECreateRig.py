51a840d6ffdd721bef116c9e77ec9136d3147cfb
Peter Chernoff <peter.chernoff@gmail.com>
2018-06-06 17:53:34 -0400

    Adds the alt00E rig that automates the entire rig creation

---
+++ pcCreateRigAlt00ECreateRig.py
@@ -0,0 +1,43 @@
+import maya.cmds as mc
+
+import pcCreateRigAlt01SpineCode
+
+reload(pcCreateRigAlt01SpineCode)
+
+import pcCreateRigAlt02HeadCode
+
+reload(pcCreateRigAlt02HeadCode)
+
+import pcCreateRigAlt03LegsCode
+
+reload(pcCreateRigAlt03LegsCode)
+
+import pcCreateRigAlt04FeetCode
+
+reload(pcCreateRigAlt04FeetCode)
+
+import pcCreateRigAlt05ArmsCode
+
+reload(pcCreateRigAlt05ArmsCode)
+
+import pcCreateRigAlt06HandsCode
+
+reload(pcCreateRigAlt06HandsCode)
+
+from pcCreateRigAlt01SpineCode import pcCreateRigAlt01SpineCode as CRA1
+from pcCreateRigAlt02HeadCode import pcCreateRigAlt02HeadCode as CRA2
+from pcCreateRigAlt03LegsCode import pcCreateRigAlt03LegsCode as CRA3
+from pcCreateRigAlt04FeetCode import pcCreateRigAlt04FeetCode as CRA4
+from pcCreateRigAlt05ArmsCode import pcCreateRigAlt05ArmsCode as CRA5
+from pcCreateRigAlt06HandsCode import pcCreateRigAlt06HandsCode as CRA6
+
+
+class pcCreateRigAlt00ECreateRig(object):
+    cra1 = CRA1()
+    cra2 = CRA2()
+    cra3 = CRA3()
+    cra4 = CRA4()
+    cra5 = CRA5()
+    cra6 = CRA6()
+
+    pass
