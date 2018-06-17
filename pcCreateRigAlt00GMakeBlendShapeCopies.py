6d3994b8e08e9cd0aefbd7be18af3bf99f7df6c8
Peter Chernoff <peter.chernoff@gmail.com>
2018-06-16 22:00:30 -0400

    This is some code I was testing out but probably will abandon

--- pcCreateRigAlt00GMakeBlendShapeCopies.py
+++ pcCreateRigAlt00GMakeBlendShapeCopies.py
@@ -1,6 +1,7 @@
-import maya.cmds as cmds
+import maya.cmds as mc
+import maya.mel as mel
 
-class pcCreateRigAlt00DRenameBlendshapeCopies():
+class pcCreateRigAlt00GMakeBlendShapeCopies():
     def __init__(self):
         mshChar = 'GEO_woman'
 
@@ -8,15 +9,47 @@ class pcCreateRigAlt00DRenameBlendshapeCopies():
 
     def tgpMakeBC(self, mshChar):
 
-        history = cmds.listHistory(mshChar)
-        # print("history {0}".format(history))
-        blndName = cmds.ls(history, typ='blendShape')[0]
-        # print("blndName {0}".format(blndName))
-        blndVals = cmds.aliasAttr(blndName, q=True)
-        # print("blndVals {0}".format(blndVals))
+        history = mc.listHistory(mshChar)
+        shape = history[0]
+        blndName = mc.ls(history, typ='blendShape')[0]
+        blndVals = mc.aliasAttr(blndName, q=True)
+        print("-----")
+        print("shape {0}".format(shape))
+        print("history {0}".format(history))
+        print("blndName {0}".format(blndName))
+        print("blndVals {0}".format(blndVals))
 
-        # takes a value that is labelled as copy and renames it to its opposite
-        for i in range(len(blndVals)):
+        weightName = [x for x in blndVals if ("weight" in x.lower())]
+        nonWeightName = [x for x in blndVals if ("weight" in x.lower())]
+        print("weightTotal: {0}".format(weightName))
+        print("nonWeightName: {0}".format(nonWeightName))
+
+
+
+        lenBnld = len(blndVals)
+
+        increment = lenBnld
+
+        for i in range(lenBnld):
+
+            blVlI = blndVals[i]
+            if "weight" in blVlI[6:]:
+                pass
+            else:
+
+                print("blndVals[{1}]: {0}".format(blndVals[i], i))
+                newName = blVlI
+                if "GEO" in blVlI:
+                    newName = "{0}_copy".format(blVlI)
+                elif "l_" in  blVlI[:2]:
+                    newName = blVlI.replace("l_", "r_")
+                mel.eval('blendShapeEditorDuplicateTargets;')
+            """
+            blendShape -e -tc on -t |GEO_woman|GEO_womanShape 22 GEO_woman1 1 -w 22 1  blendShape1;
+            blendShape -e -rtd 0 22 blendShape1;
+            aliasAttr GEO_woman_base_Copy blendShape1.w[22];
+            """
+            '''
             if "_Copy" in blndVals[i][-5:]:
                 toRename = blndVals[i][:-5]
                 if "l_" in toRename[:2]:
@@ -30,10 +63,10 @@ class pcCreateRigAlt00DRenameBlendshapeCopies():
                 print ("{0}.{1}".format(blndName, blndVals[i]))
                 if cmds.objExists('{0}.{1}'.format(blndName, toRename1)):
                     cmds.aliasAttr('{0}.{1}'.format(blndName, toRename1), rm=True)
-                cmds.aliasAttr(toRename1, '{0}.{1}'.format(blndName, blndVals[i]))
+                cmds.aliasAttr(toRename1, '{0}.{1}'.format(blndName, blndVals[i]))'''
 
                 # cmds.renameAttr( '{0}.{1}'.format(blndName, blndVals[i]), toRename1 )
 
-        blndVals = cmds.aliasAttr(blndName, q=True)
+        blndVals = mc.aliasAttr(blndName, q=True)
         print("blndVals {0}".format(blndVals))
 
